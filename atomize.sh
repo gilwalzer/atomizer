a19514e2dfa9b37c4f88a91434308fe12b70937b
Gil Walzer <gilwalzer@Interview-Epsilon.local>
2016-10-13 16:26:28 -0400

    Handles argument options

--- atomize.sh
+++ atomize.sh
@@ -5,11 +5,18 @@
 # Usage info
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-e EDITOR] [-l LANGUAGE]
+Usage: ${0##*/} [-e EDITOR] [-l LANGUAGE] [-b INCLUDE]
 
-    -e EDITOR    initialize Atom to resemble a specified text editor.
-    -l LANGUAGE  initialize Atom with convenient packages for a language.
-    -h           display commands
+    -e EDITOR     initialize Atom to resemble a specified text editor.
+                  Arguments include "sublime", "eclipse", and "all".
+
+    -l LANGUAGE   initialize Atom with convenient packages for a language.
+                  Arguments include "python", "java", "go", and "all".
+   
+    -b INCLUDE    initialize Atom with a set of basic packages.
+                  Arguments include "include".
+
+    -h            display commands
 
 EOF
 }
@@ -17,12 +24,13 @@ EOF
 # Initialize our own variables:
 editor_name=""
 language_name=""
+basic_commands=""
 
 OPTIND=1
 # Resetting OPTIND is necessary if getopts was used previously in the script.
 # It is a good idea to make OPTIND local if you process options in a function.
 
-while getopts "e:l:h" opt; do
+while getopts "b:e:l:h" opt; do
     case $opt in
         h)
             show_help
@@ -30,6 +38,9 @@ while getopts "e:l:h" opt; do
             ;;
         e)
             editor_name=$OPTARG
+            ;;
+        b)
+            basic_commands=$OPTARG
             ;;
         l)  language_name=$OPTARG
             ;;
@@ -44,7 +55,62 @@ while getopts "e:l:h" opt; do
 done
 shift "$((OPTIND-1))" # Shift off the options and optional --.
 
-# Everything that's left in "$@" is a non-option.  In our case, a FILE to process.
-printf 'editor=<%s>\nlanguage=<%s>\n' "$editor_name" "$language_name"
+# Install basic recommended atom packages
+if [[ "$basic_commands" == "include" ]]
+then
+  echo "Installing basic atom packages and themes..."
+  apm install auto-update-plus
+  apm install atom-wrap-in-tag
+  apm install atom-beautify
+  apm install autocomplete-java
+  apm install browse
+  apm install build
+  apm install set-syntax
+  apm install tag
+  apm install text-manipulation
+fi
 
-# End of file
+# Install Sublime atom packages
+if ([[ $editor_name == "sublime" ]] || [[ "$editor_name" == "all" ]])
+then
+  echo "Installing Sublime atom packages and themes..."
+  apm install minimap
+  apm install multi-cursor
+  apm install sublime
+  apm install sublime-block-comment
+  apm install sublime-panes
+  apm install sublime-word-navigation
+
+  apm install atom-monokai
+  apm install sublimify
+fi
+
+# Install Eclipse packages
+if ([[ "$editor_name" == "eclipse" ]] || [[ "$editor_name" == "all" ]])
+then
+  echo "Installing Eclipse atom packages and themes..."
+  apm install eclipse-keybindings
+  apm install cold-ui
+  apm install cold-syntax
+fi
+
+# Install Java atom packages
+if ([[ "$language_name" == "java" ]] || [[ "$language_name" == "all" ]])
+then
+  echo "Installing Java atom packages..."
+  apm install linter
+  apm install linter-javac
+fi
+
+# Install Python atom packages
+if ([[ "$language_name" == "python" ]] || [[ "$language_name" == "all" ]])
+then
+  echo "Installing Python atom packages..."
+fi
+
+# Install Go atom packages
+if ([[ "$language_name" == "go" ]] || [[ "$language_name" == "all" ]])
+then
+  echo "Installing Python atom packages..."
+fi
+
